53965141382ba89782f1c9fb8d9376e2bc5629d4
Marcel Filejecki <filej1025@gmail.com>
2023-03-09 10:06:55 +0100

    navigating with Link component

--- file-based-routing/pages/index.js
+++ file-based-routing/pages/index.js
@@ -1,9 +1,18 @@
 import React from "react";
+import Link from "next/link";
 
 const HomePage = () => {
   return (
     <div>
       <h1>The Home Page</h1>
+      <ul>
+        <li>
+          <Link href="/portfolio">Portfolio Page</Link>
+        </li>
+        <li>
+          <Link href="/clients">Clients Page</Link>
+        </li>
+      </ul>
     </div>
   );
 };
