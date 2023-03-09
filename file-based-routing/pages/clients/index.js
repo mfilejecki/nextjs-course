a0eb3c493c9c6243e8a84aafdac54238b38d84d4
Marcel Filejecki <filej1025@gmail.com>
2023-03-09 10:13:53 +0100

    navigating to dynamic routes

--- file-based-routing/pages/clients/index.js
+++ file-based-routing/pages/clients/index.js
@@ -1,9 +1,30 @@
 import React from "react";
+import Link from "next/link";
 
 const ClientsPage = () => {
+  const clients = [
+    { id: "max", name: "Maximilian" },
+    { id: "marcel", name: "Marcel" },
+  ];
+
   return (
     <div>
       <h1>The Clients Page</h1>
+      <ul>
+        <li>
+          <Link href="/clients/marek">Marek</Link>
+        </li>
+        <li>
+          <Link href="/clients/marcin">Marcin</Link>
+        </li>
+        {clients.map((client) => {
+          return (
+            <li key={client.id}>
+              <Link href={`/clients/${client.id}`}>{client.name}</Link>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 };
