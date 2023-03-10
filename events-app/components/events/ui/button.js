857bfd8fb15b59bf60666f82427d30bf3ef925d5
Marcel Filejecki <filej1025@gmail.com>
2023-03-10 13:42:48 +0100

    added button component

---
+++ events-app/components/events/ui/button.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Link from "next/link";
+import styles from "./button.module.css";
+
+const Button = (props) => {
+  return (
+    <Link className={styles.btn} href={props.link}>
+      {props.children}
+    </Link>
+  );
+};
+
+export default Button;
