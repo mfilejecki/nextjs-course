a8fe06237df5e258dd1cdc76ce1b0e33ad5147a3
Marcel Filejecki <filej1025@gmail.com>
2023-03-11 14:46:31 +0100

    small changes

--- events-app/pages/events/index.js
+++ events-app/pages/events/index.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { useRouter } from "next/router";
+import { getAllEvents } from "../../dummy-data";
+import EventList from "../../components/events/event-list";
+import EventsSearch from "../../components/events/events-search";
 
 const EventsPage = () => {
+  const router = useRouter();
+  const events = getAllEvents();
+  const findEventsDataHandler = (year, month) => {
+    const fullPath = `/events/${year}/${month}`;
+    router.push(fullPath);
+  };
   return (
-    <div>
-      <h1>EventsPage</h1>
-    </div>
+    <Fragment>
+      <EventsSearch onSearchEvents={findEventsDataHandler} />
+      <EventList items={events} />
+    </Fragment>
   );
 };
 
