31d23b83b64ea367be770554a5140d37730513a1
Marcel Filejecki <filej1025@gmail.com>
2023-03-11 13:55:02 +0100

    small changes

--- events-app/pages/events/[eventId].js
+++ events-app/pages/events/[eventId].js
@@ -1,10 +1,32 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { useRouter } from "next/router";
+import { getEventById } from "../../dummy-data";
+import EventSummary from "../../components/event-details/event-summary";
+import EventLogistics from "../../components/event-details/event-logistics";
+import EventContent from "../../components/event-details/event-content";
 
 const EventDetailPage = () => {
+  const router = useRouter();
+  const eventId = router.query.eventId;
+  const selectedEvent = getEventById(eventId);
+
+  if (!selectedEvent) {
+    return <p>Event not found.</p>;
+  }
+
   return (
-    <div>
-      <h1>EventDetailPage</h1>
-    </div>
+    <Fragment>
+      <EventSummary title={selectedEvent.title} />
+      <EventLogistics
+        date={selectedEvent.date}
+        address={selectedEvent.location}
+        image={selectedEvent.image}
+        imageAlt={selectedEvent.title}
+      />
+      <EventContent>
+        <p>{selectedEvent.description}</p>
+      </EventContent>
+    </Fragment>
   );
 };
 
