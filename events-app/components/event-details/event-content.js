a2bdd343a60c5b624822bbe3a0d9403dd7fcfd22
Marcel Filejecki <filej1025@gmail.com>
2023-03-11 13:54:29 +0100

    added a few components for displaying detail page of selected event

---
+++ events-app/components/event-details/event-content.js
@@ -0,0 +1,7 @@
+import styles from "./event-content.module.css";
+
+const EventContent = (props) => {
+  return <section className={styles.content}>{props.children}</section>;
+};
+
+export default EventContent;
