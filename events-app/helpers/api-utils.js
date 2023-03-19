a364b4e361f3780b01774f0986c3ed1db1746ff8
Marcel Filejecki <filej1025@gmail.com>
2023-03-19 13:59:46 +0100

    added helper functions for fetching events from database

---
+++ events-app/helpers/api-utils.js
@@ -0,0 +1,16 @@
+export const getAllEvents = async () => {
+  const response = await fetch(
+    "https://react-db887-default-rtdb.europe-west1.firebasedatabase.app/events.json"
+  );
+  const data = await response.json();
+  const allEvents = [];
+  for (const key in data) {
+    allEvents.push({ ...data[key], id: key });
+  }
+  return allEvents;
+};
+
+export const getFeaturedEvents = async () => {
+  const allEvents = await getAllEvents();
+  return allEvents.filter((event) => event.isFeatured);
+};
