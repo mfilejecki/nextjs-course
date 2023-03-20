d7d6452a4b54cd7a9ade2c3c301c31dceaf6b5ff
Marcel Filejecki <filej1025@gmail.com>
2023-03-20 20:00:39 +0100

    implemented api route for newsletter

--- events-app/components/input/newsletter-registration.js
+++ events-app/components/input/newsletter-registration.js
@@ -1,12 +1,29 @@
-import React from "react";
+import React, { useRef } from "react";
+import { emailSchema } from "../../validations/NewsletterValidation";
 import styles from "./newsletter-registration.module.css";
-const NewsletterRegistration = () => {
-  const registrationHandler = (event) => {
-    event.preventDefault();
 
+const NewsletterRegistration = () => {
+  const emailInputRef = useRef();
+  const registrationHandler = async (event) => {
+    event.preventDefault();
     // fetch user input (state or refs)
+    const enteredEmail = emailInputRef.current.value;
     // optional: validate input
+    const isValid = await emailSchema.isValid({ email: enteredEmail });
+    if (!isValid) {
+      console.log("Email is not valid.");
+      return;
+    }
     // send valid data to API
+    fetch("/api/newsletter", {
+      method: "POST",
+      body: JSON.stringify({ email: enteredEmail }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data));
   };
 
   return (
@@ -19,6 +36,7 @@ const NewsletterRegistration = () => {
             id="email"
             placeholder="Your email"
             aria-label="Your email"
+            ref={emailInputRef}
           />
           <button>Register</button>
         </div>
