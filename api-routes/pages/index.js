7226f3b5ba86f432eb0e7185e37096bf580ab80a
Marcel Filejecki <filej1025@gmail.com>
2023-03-20 13:59:13 +0100

    preparing frontend form

--- api-routes/pages/index.js
+++ api-routes/pages/index.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useRef } from "react";
 
 const HomePage = () => {
+  const emailInputRef = useRef();
+  const feedbackInputRef = useRef();
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const enteredEmail = emailInputRef.current.value;
+    const enteredFeedback = feedbackInputRef.current.value;
+  };
+
   return (
     <div>
       <h1>Home page</h1>
+      <form>
+        <div>
+          <label htmlFor="email">Your Email Address</label>
+          <input type="email" id="email" ref={emailInputRef} />
+        </div>
+        <div>
+          <label htmlFor="feedback">Your Feedback</label>
+          <textarea id="feedback" rows="5" ref={feedbackInputRef} />
+        </div>
+        <button>Send Feedback</button>
+      </form>
     </div>
   );
 };
