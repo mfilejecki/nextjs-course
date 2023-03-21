a4dacb222c8d7f49b8f1bbfd647494f1d6eea7aa
Marcel Filejecki <filej1025@gmail.com>
2023-03-21 22:48:18 +0100

    added error handling

--- events-app/pages/api/comments/[eventId].js
+++ events-app/pages/api/comments/[eventId].js
@@ -1,10 +1,21 @@
 import { commentsSchema } from "../../../validations/CommentsValidation";
-import { uri } from "../../../helpers/uri";
-import { MongoClient } from "mongodb";
+import {
+  connectDatabase,
+  insertDocument,
+  getAllDocuments,
+  errorResponse,
+} from "../../../helpers/db-util";
 
 const handler = async (req, res) => {
   const eventId = req.query.eventId;
-  const client = await MongoClient.connect(uri);
+
+  let client;
+  try {
+    client = await connectDatabase();
+  } catch (error) {
+    errorResponse(res, "Connecting to the database failed!");
+    return;
+  }
 
   if (req.method === "POST") {
     const { email, name, text } = req.body;
@@ -16,6 +27,7 @@ const handler = async (req, res) => {
     const isValid = commentsSchema.isValid(userInput);
     if (!isValid) {
       res.status(422).json({ message: "Invalid input." });
+      client.close();
       return;
     }
     const newComment = {
@@ -24,24 +36,28 @@ const handler = async (req, res) => {
       text,
       eventId,
     };
-    const db = client.db("events");
-    const result = await db.collection("comments").insertOne(newComment);
-    newComment.id = result.insertedId;
-
-    res
-      .status(201)
-      .json({ message: "Added new comment.", comment: newComment });
+    let result;
+    try {
+      result = await insertDocument(client, "events", "comments", newComment);
+      newComment._id = result.insertedId;
+      res
+        .status(201)
+        .json({ message: "Added new comment.", comment: newComment });
+    } catch (error) {
+      errorResponse(res, "Inserting comment to the database failed!");
+    }
   }
   if (req.method === "GET") {
-    const db = client.db("events");
-    const documents = await db
-      .collection("comments")
-      .find()
-      .sort({ _id: -1 })
-      .toArray();
-    res.status(200).json({ comments: documents });
+    let documents;
+    try {
+      documents = await getAllDocuments(client, "events", "comments", {
+        _id: -1,
+      });
+      res.status(200).json({ comments: documents });
+    } catch (error) {
+      errorResponse(res, "Getting comments from database failed!");
+    }
   }
-
   client.close();
 };
 
--- events-app/pages/api/newsletter.js
+++ events-app/pages/api/newsletter.js
@@ -1,6 +1,9 @@
 import { emailSchema } from "../../validations/NewsletterValidation";
-import { MongoClient } from "mongodb";
-import { uri } from "../../helpers/uri";
+import {
+  connectDatabase,
+  insertDocument,
+  errorResponse,
+} from "../../helpers/db-util";
 
 const handler = async (req, res) => {
   if (req.method === "POST") {
@@ -11,11 +14,23 @@ const handler = async (req, res) => {
       res.status(422).json({ message: "Invalid email address." });
       return;
     }
+    let client;
+    try {
+      client = await connectDatabase();
+    } catch (error) {
+      errorResponse(res, "Connecting to the database failed!");
+      return;
+    }
 
-    const client = await MongoClient.connect(uri);
-    const db = client.db("events");
-    await db.collection("newsletter").insertOne({ email: userEmail });
-    client.close();
+    try {
+      await insertDocument(client, "events", "newsletter", {
+        email: userEmail,
+      });
+      client.close();
+    } catch (error) {
+      errorResponse(res, "Inserting data to the database failed!");
+      return;
+    }
 
     res.status(201).json({ message: "Signed up!" });
   }
