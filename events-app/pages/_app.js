84c5706a68000312e608c2730856741dee9ecfa1
Marcel Filejecki <filej1025@gmail.com>
2023-03-22 23:42:26 +0100

    small changes

--- events-app/pages/_app.js
+++ events-app/pages/_app.js
@@ -1,16 +1,28 @@
 import "../styles/globals.css";
 import Layout from "../components/layout/layout";
 import Head from "next/head";
+import Notification from "../components/ui/notification";
+import { NotificationContextProvider } from "../store/notification-context";
 
 export default function App({ Component, pageProps }) {
   return (
-    <Layout>
-      <Head>
-        <title>Next Events</title>
-        <meta name="description" content="Kox course" />
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      </Head>
-      <Component {...pageProps} />
-    </Layout>
+    <NotificationContextProvider>
+      <Layout>
+        <Head>
+          <title>Next Events</title>
+          <meta name="description" content="Kox course" />
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          />
+        </Head>
+        <Component {...pageProps} />
+        {/* <Notification
+        title={"test"}
+        message={"this is a test"}
+        status={"pending"}
+      /> */}
+      </Layout>
+    </NotificationContextProvider>
   );
 }
