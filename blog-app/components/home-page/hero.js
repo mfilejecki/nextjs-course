34fc8da24dbc3ddd7683611b01538332a941c978
Marcel Filejecki <filej1025@gmail.com>
2023-03-23 19:53:51 +0100

    added hero component

--- blog-app/components/home-page/hero.js
+++ blog-app/components/home-page/hero.js
@@ -1,7 +1,23 @@
 import React from "react";
+import Image from "next/image";
+
+import styles from "./hero.module.css";
 
 const Hero = () => {
-  return <div></div>;
+  return (
+    <section className={styles.hero}>
+      <div className={styles.image}>
+        <Image
+          src="/images/site/femboy.jpg"
+          alt="An image of femboy."
+          width={300}
+          height={300}
+        />
+      </div>
+      <h1>Hi, I'm Marcel</h1>
+      <p>I blog about not important stuff.</p>
+    </section>
+  );
 };
 
 export default Hero;
