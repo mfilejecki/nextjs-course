43e5986e214aa93130982776fe586dd92c68b79a
Marcel Filejecki <filej1025@gmail.com>
2023-03-28 19:30:09 +0200

    dynamic post paths and pages

--- blog-app/pages/posts/[slug].js
+++ blog-app/pages/posts/[slug].js
@@ -1,9 +1,32 @@
 import React from "react";
 
 import PostContent from "../../components/posts/post-detail/post-content";
+import { getPostData, getPostFiles } from "../../lib/posts-util";
 
-const SelectedPostPage = () => {
-  return <PostContent />;
+const SelectedPostPage = (props) => {
+  return <PostContent post={props.post} />;
+};
+
+export const getStaticProps = async (context) => {
+  const { params } = context;
+  const { slug } = params;
+
+  const singlePost = getPostData(slug);
+  return {
+    props: {
+      post: singlePost,
+    },
+    revalidate: 600,
+  };
+};
+
+export const getStaticPaths = async () => {
+  const postFilenames = getPostFiles();
+  const slugs = postFilenames.map((fileName) => fileName.replace(/\.md$/, ""));
+  return {
+    paths: slugs.map((slug) => ({ params: { slug } })),
+    fallback: false,
+  };
 };
 
 export default SelectedPostPage;
