f81ff694681fb28969e49798b52642a36e9fbaa9
Marcel Filejecki <filej1025@gmail.com>
2023-03-28 19:08:35 +0200

    fetching posts with getStaticProps from markdown files

--- blog-app/pages/index.js
+++ blog-app/pages/index.js
@@ -1,38 +1,24 @@
 import React, { Fragment } from "react";
 import Hero from "../components/home-page/hero";
 import FeaturedPosts from "../components/home-page/featured-posts";
+import { getFeaturedPosts } from "../lib/posts-util";
 
-export const DUMMY_POSTS = [
-  {
-    slug: "getting-started",
-    title: "Getting Started with NextJS",
-    image: "getting-started-nextjs.png",
-    excerpt: "NextJS is a React framework for production.",
-    date: "2022-02-10",
-  },
-  {
-    slug: "getting-started2",
-    title: "Getting Started with NextJS",
-    image: "getting-started-nextjs.png",
-    excerpt: "NextJS is a React framework for production.",
-    date: "2022-02-10",
-  },
-  {
-    slug: "getting-started3",
-    title: "Getting Started with NextJS",
-    image: "getting-started-nextjs.png",
-    excerpt: "NextJS is a React framework for production.",
-    date: "2022-02-10",
-  },
-];
-
-const HomePage = () => {
+const HomePage = (props) => {
   return (
     <Fragment>
       <Hero />
-      <FeaturedPosts posts={DUMMY_POSTS} />
+      <FeaturedPosts posts={props.posts} />
     </Fragment>
   );
+};
+
+export const getStaticProps = async () => {
+  const featuredPosts = getFeaturedPosts();
+  return {
+    props: {
+      posts: featuredPosts,
+    },
+  };
 };
 
 export default HomePage;
--- blog-app/pages/posts/index.js
+++ blog-app/pages/posts/index.js
@@ -1,9 +1,19 @@
 import React from "react";
 import AllPosts from "../../components/posts/all-posts";
-import { DUMMY_POSTS } from "..";
+import { getAllPosts } from "../../lib/posts-util";
 
-const AllPostsPage = () => {
-  return <AllPosts posts={DUMMY_POSTS} />;
+const AllPostsPage = (props) => {
+  return <AllPosts posts={props.posts} />;
+};
+
+export const getStaticProps = async () => {
+  const allPosts = getAllPosts();
+
+  return {
+    props: {
+      posts: allPosts,
+    },
+  };
 };
 
 export default AllPostsPage;
