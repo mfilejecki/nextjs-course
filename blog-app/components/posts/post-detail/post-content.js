c5cdf5604cb31298848f8ddb707f911337d7c8c1
Marcel Filejecki <filej1025@gmail.com>
2023-03-28 19:28:56 +0200

    small adjusts to accept props instead of dummy data

--- blog-app/components/posts/post-detail/post-content.js
+++ blog-app/components/posts/post-detail/post-content.js
@@ -5,19 +5,13 @@ import PostHeader from "./post-header";
 
 import styles from "./post-content.module.css";
 
-const dummy_post = {
-  slug: "getting-started",
-  title: "Getting Started with NextJS",
-  image: "getting-started-nextjs.png",
-  date: "2022-02-10",
-  content: "# This is a first post",
-};
-const PostContent = () => {
-  const imagePath = `/images/posts/${dummy_post.slug}/${dummy_post.image}`;
+const PostContent = (props) => {
+  const { post } = props;
+  const imagePath = `/images/posts/${post.slug}/${post.image}`;
   return (
     <article className={styles.content}>
-      <PostHeader title={dummy_post.title} image={imagePath} />
-      <ReactMarkdown>{dummy_post.content}</ReactMarkdown>
+      <PostHeader title={post.title} image={imagePath} />
+      <ReactMarkdown>{post.content}</ReactMarkdown>
     </article>
   );
 };
