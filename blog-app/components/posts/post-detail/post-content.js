e7bbdcda01ff7872f020e9e2dbe7dbe4eb35f6cd
Marcel Filejecki <filej1025@gmail.com>
2023-03-28 21:30:44 +0200

    rendering Images with Next Image from markdown

--- blog-app/components/posts/post-detail/post-content.js
+++ blog-app/components/posts/post-detail/post-content.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactMarkdown from "react-markdown";
+import Image from "next/image";
 
 import PostHeader from "./post-header";
 
@@ -8,10 +9,23 @@ import styles from "./post-content.module.css";
 const PostContent = (props) => {
   const { post } = props;
   const imagePath = `/images/posts/${post.slug}/${post.image}`;
+
+  const customRenderers = {
+    img(image) {
+      return (
+        <Image
+          src={`/images/posts/${post.slug}/${image.src}`}
+          alt={image.alt}
+          width={600}
+          height={300}
+        />
+      );
+    },
+  };
   return (
     <article className={styles.content}>
       <PostHeader title={post.title} image={imagePath} />
-      <ReactMarkdown>{post.content}</ReactMarkdown>
+      <ReactMarkdown components={customRenderers}>{post.content}</ReactMarkdown>
     </article>
   );
 };
