8857bace7be016873d8b59032038afaf13b0df28
Marcel Filejecki <filej1025@gmail.com>
2023-03-29 18:08:21 +0200

    added two way binding and request to api

--- blog-app/components/contact/contact-form.js
+++ blog-app/components/contact/contact-form.js
@@ -1,32 +1,86 @@
-import React, { useRef } from "react";
+import React, { useState, useEffect } from "react";
+import { contactSchema } from "../../lib/contact-form-util";
 
 import styles from "./contact-form.module.css";
 
 const ContactForm = () => {
-  const emailInputRef = useRef();
-  const nameInputRef = useRef();
-  const messageInputRef = useRef();
-  const submitHandler = (event) => {
+  const [isInvalidInput, setIsInvalidInput] = useState(false);
+  const [enteredEmail, setEnteredEmail] = useState("");
+  const [enteredName, setEnteredName] = useState("");
+  const [enteredMessage, setEnteredMessage] = useState("");
+
+  useEffect(() => {
+    let timer;
+    if (isInvalidInput) {
+      timer = setTimeout(() => {
+        setIsInvalidInput(false);
+      }, 3000);
+    }
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [isInvalidInput]);
+
+  const submitHandler = async (event) => {
     event.preventDefault();
+    const userInput = {
+      email: enteredEmail,
+      name: enteredName,
+      message: enteredMessage,
+    };
+    const isValid = await contactSchema.isValid(userInput);
+
+    if (!isValid) {
+      setIsInvalidInput(true);
+      return;
+    }
+
+    fetch("/api/contact", {
+      method: "POST",
+      body: JSON.stringify(userInput),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
   };
 
   return (
     <section className={styles.contact}>
       <h1>How can I help you?</h1>
+      {isInvalidInput && (
+        <p>Invalid input, please correct it before submitting again.</p>
+      )}
       <form className={styles.form} onSubmit={submitHandler}>
         <div className={styles.controls}>
           <div className={styles.control}>
             <label htmlFor="email">Your Email</label>
-            <input type="email" id="email" ref={emailInputRef} />
+            <input
+              type="email"
+              id="email"
+              value={enteredEmail}
+              onChange={(event) => setEnteredEmail(event.target.value)}
+            />
           </div>
           <div className={styles.control}>
             <label htmlFor="name">Name</label>
-            <input type="text" id="name" ref={nameInputRef} />
+            <input
+              type="text"
+              id="name"
+              value={enteredName}
+              onChange={(event) => setEnteredName(event.target.value)}
+            />
           </div>
         </div>
         <div className={styles.control}>
           <label htmlFor="message">Your Message</label>
-          <textarea id="message" rows={5} cols={30} ref={messageInputRef} />
+          <textarea
+            id="message"
+            rows={5}
+            cols={30}
+            value={enteredMessage}
+            onChange={(event) => setEnteredMessage(event.target.value)}
+          />
         </div>
         <div className={styles.actions}>
           <button>Send Message</button>
