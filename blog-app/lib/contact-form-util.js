707fc346a21747dd1b3bc7baa6f57bace7aea171
Marcel Filejecki <filej1025@gmail.com>
2023-03-29 18:58:33 +0200

    new helper function for sending data to api

--- blog-app/lib/contact-form-util.js
+++ blog-app/lib/contact-form-util.js
@@ -5,3 +5,18 @@ export const contactSchema = yup.object().shape({
   name: yup.string().required(),
   message: yup.string().required(),
 });
+
+export const sendContactData = async (contactDetails) => {
+  const response = await fetch("/api/contact", {
+    method: "POST",
+    body: JSON.stringify(contactDetails),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (!response.ok) {
+    throw new Error(data.message || "Something went wrong!");
+  }
+  const data = await response.json();
+};
