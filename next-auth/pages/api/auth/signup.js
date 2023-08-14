cbd1982522b235401ea5b6a73c42dd4d90f6d296
Marcel Filejecki <filej1025@gmail.com>
2023-08-14 14:09:10 +0200

    feat: add nextAuth config file

---
+++ next-auth/pages/api/auth/signup.js
@@ -0,0 +1,41 @@
+import { hashPassword } from "../../../lib/auth";
+import { connectToDatabase } from "../../../lib/db";
+
+const handler = async (req, res) => {
+  if (req.method === "POST") {
+    const { email, password } = req.body;
+    if (
+      !email ||
+      !email.includes("@") ||
+      !password ||
+      password.trim().length < 7
+    ) {
+      res.status(422).json({
+        message:
+          "Invalid input - password should be at least 7 charakters long. ",
+      });
+      return;
+    }
+
+    const client = await connectToDatabase();
+    const db = client.db();
+
+    const existingUser = await db.collection("users").findOne({ email });
+
+    if (existingUser) {
+      res.status(422).json({ message: "User exists already!" });
+      client.close();
+      return;
+    }
+
+    const hashedPassword = await hashPassword(password);
+    const result = await db
+      .collection("users")
+      .insertOne({ email: email, password: hashedPassword });
+
+    res.status(201).json({ message: "Created user!" });
+    client.close();
+  }
+};
+
+export default handler;
