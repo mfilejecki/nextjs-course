a81c77cb88fd7bd8acc098a15dcf37d311232a4e
Marcel Filejecki <filej1025@gmail.com>
2023-08-14 14:34:56 +0200

    feat: add sign in possibility

--- next-auth/components/auth/auth-form.js
+++ next-auth/components/auth/auth-form.js
@@ -1,4 +1,6 @@
 import { useState, useRef } from "react";
+import { signIn } from "next-auth/client";
+
 import classes from "./auth-form.module.css";
 
 async function createUser(email, password) {
@@ -37,7 +39,13 @@ function AuthForm() {
     // optional: add validation
 
     if (isLogin) {
-      // log in user
+      const result = await signIn("credentials", {
+        redirect: false,
+        email: enteredEmail,
+        password: enteredPassword,
+      });
+
+      console.log(result);
     } else {
       try {
         const result = await createUser(enteredEmail, enteredPassword);
