9046b2178dcece1c4ff7018b3e959f13de74e49c
Marcel Filejecki <filej1025@gmail.com>
2023-08-14 16:16:09 +0200

    feat: add backend fuctionality of changing password

---
+++ next-auth/pages/api/user/change-password.js
@@ -0,0 +1,51 @@
+import { getSession } from "next-auth/client";
+import { connectToDatabase } from "../../../lib/db";
+import { hashPassword, verifyPassword } from "../../../lib/auth";
+
+async function handler(req, res) {
+  if (req.method !== "PATCH") {
+    return;
+  }
+
+  const session = await getSession({ req: req });
+
+  if (!session) {
+    res.status(401).json({ message: "Not authenticated!" });
+    return;
+  }
+
+  const userEmail = session.user.email;
+  const oldPassword = req.body.oldPassword;
+  const newPassword = req.body.newPassword;
+
+  const client = await connectToDatabase();
+  const usersCollection = client.db().collection("users");
+  const user = await usersCollection.findOne({ email: userEmail });
+
+  if (!user) {
+    res.status(404).json({ message: "User not found." });
+    client.close();
+    return;
+  }
+
+  const currentPassword = user.password;
+  const isPasswordEqual = verifyPassword(oldPassword, currentPassword);
+
+  if (!isPasswordEqual) {
+    res.status(403).json({ message: "Invalid passwords." });
+    client.close();
+    return;
+  }
+
+  const hashedNewPassword = hashPassword(newPassword);
+
+  const result = await usersCollection.updateOne(
+    { email: userEmail },
+    { $set: { password: hashedNewPassword } }
+  );
+
+  client.close();
+  res.status(200).json({ message: "Password updated." });
+}
+
+export default handler;
